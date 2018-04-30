f6b1ea42afbdf2ed2f2b8f314112e0c52912ea31
Kimminju98 <21700097@handong.edu>
2018-04-30 17:49:19 +0900

    finish prob5

--- src/main/java/edu/handong/csee/lab13/prob5/Equals.java
+++ src/main/java/edu/handong/csee/lab13/prob5/Equals.java
@@ -2,11 +2,11 @@ package edu.handong.csee.lab13.prob5;
 
 public class Equals {
 
-	public static boolean equals(int[][] list1, int[][] list2) {
+	public static boolean equals(int[][] list1, int[][] list2) { // static method, the parameters are int type array
 		
-		if(list1.length!=list2.length) return false; 
+		if(list1.length!=list2.length) return false; // if row of list1 and row of list2 are different, return false.
 		
-		int count=0;
+		int count=0; // that is used to 
 		int col=list1[0].length;
 		
 		if(list1[0].length>list2[0].length) col=list2[0].length;
