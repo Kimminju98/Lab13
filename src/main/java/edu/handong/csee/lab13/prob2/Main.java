918745697c613470a34ee3ad29c288feeef8a0ba
Kimminju98 <21700097@handong.edu>
2018-04-30 13:43:32 +0900

    finish prob3

--- src/main/java/edu/handong/csee/lab13/prob2/Main.java
+++ src/main/java/edu/handong/csee/lab13/prob2/Main.java
@@ -3,12 +3,12 @@ package edu.handong.csee.lab13.prob2;
 public class Main {
 
 	public static void main(String[]args) {
-		Book book = new Book("Simple Book");
-		Science science = new Science("Hello Physics!","ScienceWorld");
-		History history = new History("Whar Is history?","E.H.Carr");
+		Book book = new Book("Simple Book"); // instantiate Book class
+		Science science = new Science("Hello Physics!","ScienceWorld"); // instantiate Science class
+		History history = new History("Whar Is history?","E.H.Carr"); // instatiate History class
 		
-		book.show();
-		science.show();
-		history.show();
+		book.show(); // call show method of Book class
+		science.show(); // call show method of Science class
+		history.show(); // call show method of History class
 	}
 }
--- src/main/java/edu/handong/csee/lab13/prob2/Science.java
+++ src/main/java/edu/handong/csee/lab13/prob2/Science.java
@@ -1,19 +1,19 @@
 package edu.handong.csee.lab13.prob2;
 
-public class Science extends Book{
+public class Science extends Book{ // Science inherits Book
 
-	private String publisher;
+	private String publisher; // publisher
 	
-	public Science(String name, String publisher) {
-		super(name);
-		this.publisher = publisher;
+	public Science(String name, String publisher) { // constructor of Science class and the parameters are name and pulisher
+		super(name); // call the constructor of book class
+		this.publisher = publisher; // publisher of Science class is publisher of parameter
 	}
 	public void show() {
-		System.out.println("<<Science>>");
-		this.info();
+		System.out.println("<<Science>>"); // print out
+		this.info(); // call info method of Science class
 	}
 	public void info() {
-		super.info();
-		System.out.println("Publisher: "+publisher);
+		super.info(); // call info method of Book class that print out name and id
+		System.out.println("Publisher: "+publisher); // print out publisher
 	}
 }
